59b881be4102cca7b414346466e5acdd096cf1a9
Oliver <sangoliver@gmail.com>
2024-12-16 21:16:49 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+
+let multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 100) + 1
+
+const num3 = 10
+const num4 = 6
+const num5 = 20
+
+const mod = num3 % num4
+
+const max = Math.max(num3, num4, num5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
